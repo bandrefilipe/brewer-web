98a0f91a3578af86f1b57be951df48cff90c5b64
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-07 16:36:10 -0300

    create brewer web project

---
+++ src/main/java/bandrefilipe/brewer/web/BrewerWebApplication.java
@@ -0,0 +1,12 @@
+package bandrefilipe.brewer.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewerWebApplication.class, args);
+    }
+}
