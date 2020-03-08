b67bea0076d0df1ddbc1ce07e28e24ac7d0560ec
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-08 12:43:50 -0300

    create controller specific constants

---
+++ src/main/java/bandrefilipe/brewer/web/controller/API.java
@@ -0,0 +1,13 @@
+package bandrefilipe.brewer.web.controller;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Package-visible Web API constants.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final class API {
+
+    public static final String BEVERAGES = "/beverages";
+}
