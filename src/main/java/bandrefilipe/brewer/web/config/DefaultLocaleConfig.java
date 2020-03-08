12084b8e90992cbce2d891897d514502739eafe8
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-08 12:42:01 -0300

    create default locale bean configuration

---
+++ src/main/java/bandrefilipe/brewer/web/config/DefaultLocaleConfig.java
@@ -0,0 +1,39 @@
+package bandrefilipe.brewer.web.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Configures the default locale, as returned by {@link Locale#getDefault()}.
+ */
+@Slf4j
+@Configuration
+class DefaultLocaleConfig {
+
+    @Value("${locale.default}")
+    private String defaultLocale;
+
+    private final Map<String, Locale> validLocales;
+
+    DefaultLocaleConfig() {
+        validLocales = new HashMap<>();
+        validLocales.put("en_US", Locale.US);
+        validLocales.put("es", new Locale("es"));
+        validLocales.put("pt_BR", new Locale("pt", "BR"));
+    }
+
+    @Bean
+    Locale defaultLocale() {
+        log.debug("Creating bean 'defaultLocale'");
+        Locale.setDefault(validLocales.getOrDefault(defaultLocale, Locale.US));
+        final var result = Locale.getDefault();
+        log.debug("Default locale set to {}", result);
+        return result;
+    }
+}
