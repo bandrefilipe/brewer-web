73d0528a29e1c850534899810cb1d60519437d62
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-08 12:43:50 -0300

    create controller specific constants

---
+++ src/main/java/bandrefilipe/brewer/web/controller/Redirect.java
@@ -0,0 +1,13 @@
+package bandrefilipe.brewer.web.controller;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Package-visible Spring Web MVC redirect constants.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final class Redirect {
+
+    public static final String BEVERAGES_NEW = "redirect:/beverages/new";
+}
---
+++ src/main/java/bandrefilipe/brewer/web/controller/View.java
@@ -0,0 +1,13 @@
+package bandrefilipe.brewer.web.controller;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Package-visible Spring Web MVC view constants.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+final class View {
+
+    public static final String BEVERAGE_REGISTRATION = "beverage/registration";
+}
