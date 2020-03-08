4bc3b343eacfcf4ba182cf42279d972131afc35d
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-08 12:44:15 -0300

    create beverage model

---
+++ src/main/java/bandrefilipe/brewer/web/model/Beverage.java
@@ -0,0 +1,19 @@
+package bandrefilipe.brewer.web.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public final class Beverage {
+
+    @NotBlank(message = "{model.beverage.validation.sku.not-blank}")
+    private String sku;
+
+    @NotBlank(message = "{model.beverage.validation.name.not-blank}")
+    private String name;
+
+    @Size(max = 50, message = "{model.beverage.validation.description.size}")
+    private String description;
+}
