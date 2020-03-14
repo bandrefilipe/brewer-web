268cbffb6d716be7ff584dcfa88e9df612052af4
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-14 11:54:39 -0300

    add default locale setup in the main method

--- src/main/java/bandrefilipe/brewer/web/BrewerWebApplication.java
+++ src/main/java/bandrefilipe/brewer/web/BrewerWebApplication.java
@@ -3,10 +3,15 @@ package bandrefilipe.brewer.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
+import static bandrefilipe.brewer.web.GlobalConstants.LOCALE_EN_US;
+
 @SpringBootApplication
 public class BrewerWebApplication {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
+        Locale.setDefault(LOCALE_EN_US);
         SpringApplication.run(BrewerWebApplication.class, args);
     }
 }
