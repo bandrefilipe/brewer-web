45595fe12f222c7db97447d057d99b13119408db
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-14 11:55:01 -0300

    add javadoc documentation

--- src/main/java/bandrefilipe/brewer/web/core/ValidationErrors.java
+++ src/main/java/bandrefilipe/brewer/web/core/ValidationErrors.java
@@ -4,6 +4,9 @@ import org.springframework.validation.Errors;
 
 import java.util.Set;
 
+/**
+ * Provides helper methods to handle validation messages.
+ */
 public interface ValidationErrors {
 
     Set<String> getMessages(Errors errors);
