91b54be000eaa1a0fd2f22b17690fae7c28a829a
Andre Barranco <b.andrefilipe@gmail.com>
2020-03-21 01:58:04 -0300

    move implementations to impl package

--- src/main/java/bandrefilipe/brewer/web/core/impl/MessageSourceImpl.java
+++ src/main/java/bandrefilipe/brewer/web/core/impl/MessageSourceImpl.java
@@ -1,4 +1,4 @@
-package bandrefilipe.brewer.web.core;
+package bandrefilipe.brewer.web.core.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
